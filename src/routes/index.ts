831222df90a71b0d5030bffd2ef88927e52531b9
pawelsikowski <pawel.sikowski@arsokomputery.pl>
2020-06-24 17:49:27 +0200

    replies body properly returned

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,27 @@
-import { Router, urlencoded } from 'express';
+import { Router } from 'express';
+import TweetPuppeteer from '../services/TweetPuppeteer';
 const router = Router();
 
-router.get('/api/tweet/', (req, res) => {
+router.get('/api/tweet/', async (req, res) => {
+  let url: string;
+  let count: number;
+  let responseUrl: any;
   try {
     if (req.query.url) {
-      const url = req.query.url;
+      url = req.query.url.toString();
     } else throw Error('invalid url');
     if (req.query.count) {
-      const count = req.query.url;
+      count = parseInt(req.query.url.toString());
     } else throw Error('invalid count');
+    responseUrl = await TweetPuppeteer.getTweetData(url, count);
+    console.log(responseUrl);
   } catch (error) {
     res.status(400).json({
       message: error.message,
     });
   }
 
-  res.send('Hello world!');
+  res.json(responseUrl);
 });
 
 export default router;
--- src/services/TweetPuppeteer.ts
+++ src/services/TweetPuppeteer.ts
@@ -1,1 +1,51 @@
 import puppeteer from 'puppeteer';
+
+class Post {
+  favourite_count = 0;
+  full_text = '';
+  quote_count = 0;
+  reply_count = 0;
+  retweet_count = 0;
+}
+
+export default {
+  async getTweetData(url: string, count: number): Promise<any> {
+    let responseBody: any;
+    const browser = await puppeteer.launch({ headless: false });
+    const [page] = await browser.pages();
+    // page.setRequestInterception(true);
+    // page.on('request', (interceptedRequest) => {
+    //   if (this.isValidDataUrl(interceptedRequest.url())) {
+    //     interceptedRequest.continue({
+    //       url: interceptedRequest
+    //         .url()
+    //         .replace(/&count=\d+&/, `&count=${count}&`),
+    //     });
+    //   }
+    //   interceptedRequest.continue();
+    // });
+    page.on('requestfinished', async (request) => {
+      console.log('gotowy request');
+      if (
+        this.isValidDataUrl(request.url()) &&
+        request.response()?.headers()['content-length'] !== '0'
+      ) {
+        console.log('prawidłowy request');
+        responseBody = await request.response()?.json();
+      }
+    });
+    await page.goto(url);
+    await page.waitForSelector('[role=region]');
+    browser.close();
+    return new Promise<any>((resolve) => {
+      console.log('jestem! ' + responseBody);
+      resolve(responseBody);
+    });
+  },
+
+  isValidDataUrl(url: string): boolean {
+    return /^https:\/\/api.twitter.com\/\d+\/timeline\/conversation\/\d+.json/.test(
+      url
+    );
+  },
+};
