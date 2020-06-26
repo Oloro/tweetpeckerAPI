c37594751908d47dd089b262a9081757dcfe2106
pawelsikowski <pawel.sikowski@arsokomputery.pl>
2020-06-26 14:07:20 +0200

    env and cors (not working properly)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import TweetPuppeteer from '../services/TweetPuppeteerService';
+import Post from '../models/Post';
 const router = Router();
 
 router.get('/api/tweet/', async (req, res) => {
   let url: string;
   let count: number;
-  let replies: any;
+  let replies: Post[];
   try {
     if (req.query.url) {
       url = req.query.url.toString();
@@ -14,13 +15,12 @@ router.get('/api/tweet/', async (req, res) => {
       count = parseInt(req.query.count.toString());
     } else throw Error('invalid count');
     replies = await TweetPuppeteer.getTweetData(url, count);
+    res.json(replies);
   } catch (error) {
     res.status(400).json({
       message: error.message,
     });
   }
-
-  res.json(replies);
 });
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,26 @@
 import express from 'express';
 import router from './routes';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  methods: 'GET',
+  allowedHeaders: 'Content-Type',
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+};
+
+dotenv.config();
 const app: express.Application = express();
 
+app.use(cors());
 app.use(router);
 
-app.listen(51253, () => {
-  console.log('Listening on 51253...');
-});
+if (process.env.HOST && process.env.PORT) {
+  app.listen(parseInt(process.env.PORT), process.env.HOST, () => {
+    console.log(`Listening on ${process.env.HOST}:${process.env.PORT}...`);
+  });
+} else {
+  console.log('Required environmental variables not found.');
+}
